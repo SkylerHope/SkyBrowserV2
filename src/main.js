6e1df58d36952b5db0300ca4813c4d068dadb12a
SkylerHope <elpidakouro13@gmail.com>
2024-08-13 02:20:07 +0300

    basic search function working

--- src/main.js
+++ src/main.js
@@ -1,18 +1,6 @@
-const { invoke } = window.__TAURI__.tauri;
-
-let greetInputEl;
-let greetMsgEl;
-
-async function greet() {
-  // Learn more about Tauri commands at https://tauri.app/v1/guides/features/command
-  greetMsgEl.textContent = await invoke("greet", { name: greetInputEl.value });
-}
-
-window.addEventListener("DOMContentLoaded", () => {
-  greetInputEl = document.querySelector("#greet-input");
-  greetMsgEl = document.querySelector("#greet-msg");
-  document.querySelector("#greet-form").addEventListener("submit", (e) => {
-    e.preventDefault();
-    greet();
-  });
-});
+document.getElementById('search-form').addEventListener('submit', function(event) {
+  event.preventDefault();
+  var query = document.getElementById('search-bar').value;
+  var search = 'https://www.google.com/search?q=' + encodeURIComponent(query);
+  window.location.href = search;
+})
